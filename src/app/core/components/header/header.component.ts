7b226c6ddc6d883ba784f80028d62eeef05260dd
andre <andredff@gmai.com>
2025-10-07 15:33:58 -0300

    feat: adding count badge

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { FavoritesService } from '../../services/favorites.service';
 import { map, Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common'; // Importação do AsyncPipe
+import { AsyncPipe } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonComponent, AsyncPipe], // Adicione o AsyncPipe aqui
+  imports: [ButtonComponent, AsyncPipe],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
@@ -16,7 +16,6 @@ export class HeaderComponent {
   favoritesCount$: Observable<number>;
 
   constructor(private favoritesService: FavoritesService, public router: Router) {
-    // Mapeia o Observable para obter a contagem de favoritos
     this.favoritesCount$ = this.favoritesService.favorites$.pipe(
       map((favorites) => favorites.length)
     );
