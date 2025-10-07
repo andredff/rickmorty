effb13130dc552b078614b52ba291f73a076b44c
andre-zexter <andre@zexter.com>
2025-10-06 23:25:12 -0300

    feat: adding favorites component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './core/pages/home/home.component';
+import { FavoritesComponent } from './core/pages/favorites/favorites.component';
 
 export const routes: Routes = [
   {
-    path: '**',
+    path: '',
     component: HomeComponent
+  },
+  {
+    path: 'favoritos',
+    component: FavoritesComponent
   }
 ];
---
+++ src/app/core/pages/favorites/favorites.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TitleComponent } from '../../components/title/title.component';
+
+@Component({
+  selector: 'app-favorites.component',
+  imports: [TitleComponent],
+  templateUrl: './favorites.component.html',
+  styleUrl: './favorites.component.scss'
+})
+export class FavoritesComponent {
+
+}
