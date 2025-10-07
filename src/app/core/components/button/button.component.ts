151cb4c83baaba0b8ec274eb783df85190ab8657
andre <andredff@gmai.com>
2025-10-07 15:27:26 -0300

    feat: adding count badge

--- src/app/core/components/button/button.component.ts
+++ src/app/core/components/button/button.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, input, OnInit } from '@angular/core';
+import { Component, Input, input, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,37 +9,19 @@ import { RouterLink } from '@angular/router';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent implements OnInit {
+  @Input() buttonClass: string = '';
   label = input<string>();
   icon = input<string>();
   count = input<number | undefined>();
   link = input<string | string[]>();
+  isActive = input<boolean>(false);
   type = input<string>('button');
-  buttonType = input<'primary' | 'secondary'>('primary');
-  borderRightRadius = input<boolean | undefined>(true)
-  borderLeftRadius = input<boolean | undefined>(true);
+
 
   ngOnInit(): void {
     if (!this.label) {
       throw new Error('The label input is required');
-
     }
   }
 
-  private buttonClasses: Record<string, string> = {
-    primary: 'primary-button',
-    secondary: 'secondary-button'
-  };
-
-  get classeCss(): string {
-    return this.buttonClasses[this.buttonType()] || 'primary-button';
-  }
-
-  get dynamicStyles(): any {
-    return {
-      'border-top-right-radius': this.borderRightRadius() ? '12px' : '0',
-      'border-bottom-right-radius': this.borderRightRadius() ? '12px' : '0',
-      'border-top-left-radius': this.borderLeftRadius() ? '12px' : '0',
-      'border-bottom-left-radius': this.borderLeftRadius() ? '12px' : '0'
-    };
-  }
 }
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { ButtonComponent } from '../button/button.component';
 import { FavoritesService } from '../../services/favorites.service';
 import { map, Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common'; // Importação do AsyncPipe
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -14,10 +15,15 @@ import { AsyncPipe } from '@angular/common'; // Importação do AsyncPipe
 export class HeaderComponent {
   favoritesCount$: Observable<number>;
 
-  constructor(private favoritesService: FavoritesService) {
+  constructor(private favoritesService: FavoritesService, public router: Router) {
     // Mapeia o Observable para obter a contagem de favoritos
     this.favoritesCount$ = this.favoritesService.favorites$.pipe(
       map((favorites) => favorites.length)
     );
+  }
+
+  checkActiveRoute(route: string): boolean {
+    console.log(this.router.url === route)
+    return this.router.url === route;
   }
 }
