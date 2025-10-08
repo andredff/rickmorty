3d45dca3e53d8559dd04a797c9c5b3993a939221
andre <andredff@gmai.com>
2025-10-08 13:35:40 -0300

    fix: character-detail mobile button

--- src/app/core/components/character-card/character-card.component.ts
+++ src/app/core/components/character-card/character-card.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TruncatePipe } from '../../pipes/truncate.pipe';
 import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-character-card',
   standalone: true,
-  imports: [CommonModule, TruncatePipe, RouterLink],
+  imports: [CommonModule, TruncatePipe, RouterLink, TranslatePipe],
   templateUrl: './character-card.component.html',
   styleUrls: ['./character-card.component.scss'],
 })
